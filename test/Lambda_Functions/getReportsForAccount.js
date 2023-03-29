8e43d5ad066ed7bf423c00cb91c70f28435d0daa
Collin Vesel <55296006+testmaster217@users.noreply.github.com>
2023-03-29 16:33:00 +0000

    Added a Lambda to get all reports for a given account_id.

---
+++ test/Lambda_Functions/getReportsForAccount.js
@@ -0,0 +1,34 @@
+var AWS = require('aws-sdk');
+var documentClient = new AWS.DynamoDB.DocumentClient({apiVersion: '2012-08-10'});
+const tableName = "Report";
+exports.handler = async event => {
+    // TODO implement
+    console.log("Received: " + JSON.stringify(event, null, 2));
+    let response = "";
+    
+    try {
+        var params = {
+          TableName : tableName,
+          FilterExpression : 'account_id = :the_account_id',
+          ExpressionAttributeValues : {
+            ':the_account_id' : event.headers.account_id
+          },
+        };
+        
+        const reports = await documentClient.scan(params).promise();
+        
+        response = {
+            "statusCode": 200,
+            "body": JSON.stringify(reports),
+        };       
+        
+    } catch (exception) {
+        console.error(exception);
+        response = {
+            "statusCode": 500,
+            "body": JSON.stringify({"Message: " : exception}),
+        };
+    }
+    
+    return response;
+};
