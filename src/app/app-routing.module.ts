7425ba6c01b57b1badde477e1d5d77bd3144b8df
treyucm <122303761+treyucm@users.noreply.github.com>
2023-04-05 17:09:12 +0000

    modified:   src/app/app-routing.module.ts 	modified:   src/app/app.component.html 	modified:   src/app/app.module.ts 	modified:   src/app/login/login.component.html 	modified:   src/app/top-bar/top-bar.component.html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { DataTableComponent } from './data-table/data-table.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login-component', component: LoginComponent},
+  { path: 'data-table-component', component: DataTableComponent },
+  { path: 'top-bar-component', component: TopBarComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
