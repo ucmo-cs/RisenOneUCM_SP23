5912361679aacc86739803bdd2a5668edbd549b4
BrandonDobson <97484589+BrandonDobson@users.noreply.github.com>
2023-04-10 17:06:58 +0000

    modified:   src/app/login/login.component.ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { SocialAuthService } from '@abacritt/angularx-social-login';
 import { Router } from '@angular/router';
 import { GoogleLoginProvider } from '@abacritt/angularx-social-login';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -16,4 +17,9 @@ export class LoginComponent {
   callbackFunction(){
     console.log("TEST");
   }
+
+  loginForm: FormGroup = new FormGroup({
+    email: new FormControl(null, [Validators.required, Validators.email]),
+    password: new FormControl(null, [Validators.required]),
+  });
 }
