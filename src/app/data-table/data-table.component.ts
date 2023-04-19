ad5378a7e321a845132d2aa062e465dd34305b20
Judylimo <48482152+Judylimo@users.noreply.github.com>
2023-04-19 16:48:21 +0000

    modified:   src/app/data-table/data-table.component.ts

--- src/app/data-table/data-table.component.ts
+++ src/app/data-table/data-table.component.ts
@@ -123,7 +123,7 @@ export class DataTableComponent implements OnInit, OnDestroy {
                   /*Disables page and then updates report*/
                   this.toggleLayer = true;
                   //await this.delay(1500);
-                  this.addreportService.saveReport(reportData).subscribe();
+                  //this.addreportService.saveReport(reportData).subscribe();
                   await this.delay(500);
                   //location.reload();
                   count++;
